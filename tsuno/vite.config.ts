e706a158af7c44bd97f001bfc00e12c02deb0313
cronJohn <51012201+cronJohn@users.noreply.github.com>
2024-06-14 23:23:01 -0500

    Add vitest support to tsuno

--- tsuno/vite.config.ts
+++ tsuno/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
@@ -15,7 +18,17 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['node_modules/@testing-library/jest-dom/vitest'],
+    // if you have few tests, try commenting this out to improve performance:
+    isolate: false,
+  },
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    conditions: ['development', 'browser'],
   },
 });
